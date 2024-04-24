478b123c9a8e0418f7c3115183cb2fd71a95b575
Sofie Buur <82093368+BuurPrime@users.noreply.github.com>
2024-04-24 10:50:55 +0200

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
-import Game from './components/Game';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Game from "./components/Game";
 
 function App() {
   return (
-    <div className="App">
-      <Game/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Game />} />
+      </Routes>
+    </Router>
   );
 }
 
