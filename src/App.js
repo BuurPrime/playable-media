aacd3a36bd88c769d3289b939cf0735fa9f027d8
BuurPrime <sofieamaliebuur@gmail.com>
2024-04-27 12:12:03 +0200

    added welcome and routing + fixed global styling issues

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Game from "./components/Game";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
-    <div className="App">
-      <Game/>
-    </div>
+    <Router>
+      <Routes >
+        <Route path="/" element={<Game />} className="App"/>
+        <Route path="/welcome" element={<Welcome />} className="App"/>
+        <Route path="/game" element={<Game />} className="App"/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/welcome.js
@@ -0,0 +1,12 @@
+import { React, useState } from 'react';
+import './Welcome.css';
+
+function Welcome() {
+  return (
+    <div>
+        <p> HI </p>
+    </div>
+  );
+}
+
+export default Welcome;
