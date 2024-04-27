b8d099b11fb7de329fab984440746f16002f2e72
BuurPrime <sofieamaliebuur@gmail.com>
2024-04-27 11:44:29 +0200

    Made all rooms, and added error message

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,24 +6,52 @@ function Game() {
     bedroom: {
       name: "bedroom",
       roomDescription: "This is your bedroom",
-      lookAround: "THe cage of Waffles is quiet",
+      lookAround: "The cage of Waffles is quiet",
       exits: ["living room"],
-      items: ["Waffles body"],
+      items: ["Waffles body"]
     },
     livingRoom: {
       name: "living room",
       roomDescription: "This is your living room",
       lookAround: "sunflowers in the vase",
-      exits: ["bedroom", "bathroom", "kitchen", "office", "entry"],
-      items: ["sunflower seeds"],
+      exits: ["bedroom", "bathroom", "kitchen", "entry"],
+      items: ["sunflower seeds"]
     },
     bathroom: {
       name: "bathroom",
       roomDescription: "This is your bathroom",
       lookAround: "The towel is wet from yesterday",
       exits: ["living room"],
-      items: ["shampoo"],
+      items: ["shampoo"]
     },
+    kitchen: {
+      name: "kitchen",
+      roomDescription: "This is your kitchen",
+      lookAround: "All over the table is a mess the from making a cake... You do not recall it being there",
+      exits: ["living room", "closet"],
+      items: ["shampoo"]
+    },
+    closet: {
+      name: "closet",
+      roomDescription: "A broom closet.",
+      lookAround: "Yup there is a broom in here.",
+      exits: ["kitchen"],
+      items: ["pen"]
+    },
+    office: {
+      name: "office",
+      roomDescription: "This is your office!",
+      lookAround: "You work form home alot, which is also why you don't recall leaving",
+      exits: ["entry"],
+      items: ["pen"]
+    },
+    entry: {
+      name: "entry",
+      roomDescription: "This is your office!",
+      lookAround: "You work form home alot, which is also why you don't recall leaving",
+      exits: ["living room", "office"],
+      items: ["pen"]
+    }
   };
 
   const [currentRoom, setCurrentRoom] = useState(rooms["bedroom"]);
@@ -33,6 +61,7 @@ function Game() {
   );
   const [input, setInput] = useState("");
   const [inventory, setInventory] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
   //useEffect(() => {}, [currentRoom]);
 
@@ -56,14 +85,59 @@ function Game() {
     console.log("Go command given: " + { input });
     if (
       input.toLowerCase().trim().endsWith("living room") &&
-      rooms.bathroom.exits.includes("living room")
+      currentRoom.exits.includes("living room")
     ) {
       setCurrentRoom(rooms["livingRoom"]);
       setDescription(rooms["livingRoom"].roomDescription);
-    } else if (input.toLowerCase().trim().endsWith("bathroom")) {
+      setErrorMessage("");
+    } else if(input.toLowerCase().includes(currentRoom.name.toLowerCase())){
+      setErrorMessage("You are already here!");
+    } else if (
+      input.toLowerCase().trim().endsWith("bathroom") &&
+      currentRoom.exits.includes("bathroom")
+    ) {
       setCurrentRoom(rooms["bathroom"]);
       setDescription(rooms["bathroom"].roomDescription);
+      setErrorMessage("");
+    } else if (
+      input.toLowerCase().trim().endsWith("kitchen") &&
+      currentRoom.exits.includes("kitchen")
+    ) {
+      setCurrentRoom(rooms["kitchen"]);
+      setDescription(rooms["kitchen"].roomDescription);
+      setErrorMessage("");
+    } else if (
+      input.toLowerCase().trim().endsWith("closet") &&
+      currentRoom.exits.includes("closet")
+    ) {
+      setCurrentRoom(rooms["closet"]);
+      setDescription(rooms["closet"].roomDescription);
+      setErrorMessage("");
+    } else if (
+      input.toLowerCase().trim().endsWith("office") &&
+      currentRoom.exits.includes("office")
+    ) {
+      setCurrentRoom(rooms["office"]);
+      setDescription(rooms["office"].roomDescription);
+      setErrorMessage("");
+    } else if (
+      input.toLowerCase().trim().endsWith("entry") &&
+      currentRoom.exits.includes("entry")
+    ) {
+      setCurrentRoom(rooms["entry"]);
+      setDescription(rooms["entry"].roomDescription);
+      setErrorMessage("");
+    } else if (
+      input.toLowerCase().trim().endsWith("bedroom") &&
+      currentRoom.exits.includes("bedroom")
+    ) {
+      setCurrentRoom(rooms["bedroom"]);
+      setDescription(rooms["bedroom"].roomDescription);
+      setErrorMessage("");
     }
+    
+    else{handleInvalidInput();}
+
   };
 
   const handleUseCommand = () => {
@@ -72,6 +146,7 @@ function Game() {
 
   const handleInvalidInput = () => {
     console.log("not a valid command.");
+    setErrorMessage("Not a valid command. Please check your spelling, or click 'help' in the top left corner.");
   };
 
   const handleKeyPress = (event) => {
@@ -115,6 +190,7 @@ function Game() {
         onKeyUp={handleKeyPress}
         placeholder="Enter command..."
       />
+      <p className="error-message"> {errorMessage} </p>
     </div>
   );
 }
