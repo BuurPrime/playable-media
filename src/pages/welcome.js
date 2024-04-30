bbbfd5d91e68394501f3bee86f6d01321e306af1
BuurPrime <sofieamaliebuur@gmail.com>
2024-04-30 15:03:30 +0200

    added pop up for how to play

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -1,4 +1,4 @@
-import {React, useState} from "react";
+import { React, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion"; // Import motion from framer-motion
 import "./Welcome.css";
@@ -9,15 +9,24 @@ function HowToPlayPopUp({ onClose }) {
   return (
     <div className="popup">
       <div className="popup-content">
-        <h2>Popup Content</h2>
-        <p>This is the content of the popup.</p>
-        <button onClick={onClose}>Close</button>
+        <h2> How to play </h2>
+        <p> This is a text adventue game. </p>
+        <p> You can use commands including: </p>
+        <ul>
+          <li> Look around: Examine your surroundings. </li>
+          <li> Go [room]: Move to a different room. </li>
+          <li> Take [item]: Pick up an item. </li>
+          <li> Use [item]: Interact with an item. </li>
+          <li> Inventory: Check your inventory. </li>
+          <li> Help: Get assistance with commands. </li>
+        </ul>
+        <button className="button" onClick={onClose}>
+          Close
+        </button>
       </div>
     </div>
   );
 }
-
-
 
 function Welcome() {
   const navigate = useNavigate();
@@ -25,7 +34,7 @@ function Welcome() {
   const startGame = () => {
     navigate("/PreGame");
   };
-  
+
   const [isPopupOpen, setIsPopupOpen] = useState(false);
 
   const togglePopup = () => {
@@ -36,21 +45,23 @@ function Welcome() {
     setIsPopupOpen(false);
   };
 
-
   return (
     <motion.div
-    initial={{ opacity: 0}}
-    animate={{ opacity: 1}}
-    exit={{ opacity: 0}}
-    transition={{ duration: 1, delay: 0.2 }}
-    className="outer-container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1, delay: 0.2 }}
+      className="outer-container"
     >
       <div className="welcome-content">
         <h1> IN THE VASE SITS PRETTY FLOWERS </h1>
         <button className="button start" onClick={startGame}>
           START GAME
         </button>
-        <button onClick={togglePopup} className="button how-to-play"> How to play </button>
+        <button onClick={togglePopup} className="button how-to-play">
+          {" "}
+          How to play{" "}
+        </button>
         {isPopupOpen && <HowToPlayPopUp onClose={closePopup} />}
       </div>
     </motion.div>
